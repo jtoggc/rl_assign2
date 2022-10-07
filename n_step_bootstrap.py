36e7da500c5d86aacf2c625350e7fd11a65ef7a0
Josh Orrick <joshorrick@Joshs-MacBook-Air.local>
2022-10-07 01:28:39 -0400

    First tries at n-step TD and Sarsa, passes tests in test.py but fails grader

--- n_step_bootstrap.py
+++ n_step_bootstrap.py
@@ -4,6 +4,28 @@ import numpy as np
 from env import EnvSpec
 from policy import Policy
 
+
+class GreedyPolicy(Policy):
+    def __init__(self, Q, nA, nS):
+        self.p = np.array([[1/nA]*nA]*nS)
+        self.Q = Q
+        # for i in range(len(self.Q)):
+        #     action_idx = np.argmax(self.Q[i,:]) # action with largest Q value
+        #     self.p[i, action_idx] = 1.0 # full probability for largest Q value
+
+    def update_policy(self, Q: np.array) -> None:
+        for i in range(len(Q)):
+            action_idx = np.argmax(Q[i,:]) # action with largest Q value
+            self.p[i, action_idx] = 1.0 # full probability for largest Q value, 0 else
+            self.p[i, [y for y in range(self.p.shape[1]) if y != action_idx]] = 0.0
+
+    def action_prob(self, state: int, action: int) -> float:
+        return self.p[state, action]
+
+    def action(self, state: int) -> int:
+        return np.argmax(self.p[state])
+
+
 def on_policy_n_step_td(
     env_spec:EnvSpec,
     trajs:Iterable[Iterable[Tuple[int,int,int,int]]],
@@ -28,6 +50,20 @@ def on_policy_n_step_td(
     # sampling (Hint: Sutton Book p. 144)
     #####################
 
+    V = initV
+    for episode in trajs:
+        T = len(episode)
+        for t in range(T):
+            tau = t-n+1
+            if tau >= 0:
+                G = 0
+                for i in range(tau+1, min(tau+n, T-1)+1):
+                    G += pow(env_spec.gamma, i-tau-1)*episode[i][2] # reward is at second tuple index in each step
+                if tau+n < T:
+                    G += pow(env_spec.gamma, n)*V[episode[tau+n][0]] # could be indexing error here for V(s_tau+n)
+                V[episode[tau][0]] += alpha*(G - V[episode[tau][0]])
+            if tau == T-1:
+                break
     return V
 
 def off_policy_n_step_sarsa(
@@ -56,5 +92,26 @@ def off_policy_n_step_sarsa(
     # TODO: Implement Off Policy n-Step SARSA algorithm
     # sampling (Hint: Sutton Book p. 149)
     #####################
+
+    Q = initQ
+    pi = GreedyPolicy(Q, env_spec.nA, env_spec.nS)
+
+    for episode in trajs:
+        T = len(episode)
+        for t in range(T):
+            tau = t-n+1
+            if tau >= 0:
+                rho = 1
+                G = 0
+                for k in range(tau+1, min(tau+n, T-2) + 1):
+                    rho *= pi.action_prob(episode[k][0], episode[k][1])/bpi.action_prob(episode[k][0], episode[k][1])
+                for j in range(tau+1, min(tau+n, T-1) + 1):
+                    G += pow(env_spec.gamma, j-tau-1)*episode[j][2]
+                if tau+n < T:
+                    G += pow(env_spec.gamma, n)*Q[episode[tau+n][0], episode[tau+n][1]]
+                Q[episode[tau][0], episode[tau][1]] += alpha*rho*(G - Q[episode[tau][0], episode[tau][1]])
+                pi.update_policy(Q)
+            if tau == T-1:
+                break
 
     return Q, pi
